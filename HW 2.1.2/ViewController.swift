6736356951439ec507f6b62f5fad714826a9bbd7
Aquesta <gkrasnoshlyk@gmail.com>
2020-08-20 13:39:54 +0500

    HW 2.1.2

--- HW 2.1.2/ViewController.swift
+++ HW 2.1.2/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
+        
+        redView.alpha = 0.3
+        greenView.alpha = 0.3
+        yellowView.alpha = 0.3
     }
 
-
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        changeViewAlpha(red: redView, green: greenView, yellow: yellowView)
+        }
+    
+    func changeViewAlpha (red: UIView, green: UIView, yellow: UIView) {
+        if red.alpha.rounded() == 0.0 && green.alpha.rounded() == 0.0 && yellow.alpha.rounded() == 0.0 {
+            red.alpha = 1
+        } else if red.alpha == 1.0 {
+            red.alpha = 0.3
+            yellow.alpha = 1
+        } else if yellow.alpha == 1 {
+            yellow.alpha = 0.3
+            green.alpha = 1
+        } else {
+            red.alpha = 1
+            green.alpha = 0.3
+        }
+    }
 }
 
